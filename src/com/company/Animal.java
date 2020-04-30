522e2f3e01e8b874b7394c1af7810aed97d04e6c
frpiotrowicz <62611565+frpiotrowicz@users.noreply.github.com>
2020-04-30 16:54:24 +0200

    zadanie 0

---
+++ src/com/company/Animal.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Animal {
+    String name;
+    String species;
+    Double weight;
+}
---
+++ src/com/company/Human.java
@@ -0,0 +1,6 @@
+package com.company;
+
+public class Human {
+    String fName;
+    String lName;
+}
