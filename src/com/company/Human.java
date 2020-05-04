779447898fc3a2e59d0a2eacfcb9e99d5d11f611
frpiotrowicz <62611565+frpiotrowicz@users.noreply.github.com>
2020-05-04 22:29:19 +0200

    getter z data

--- src/com/company/Human.java
+++ src/com/company/Human.java
@@ -1,8 +1,30 @@
 package com.company;
 
+import java.time.LocalDateTime;
+import java.util.Date;
+
 public class Human {
     String fName;
     String lName;
     Animal pet;
     Car car;
+
+    private Double salary = 100.0;
+    private LocalDateTime date;
+    private Double prevSalary;
+
+
+    public Double getSalary() {
+        System.out.println("last opened " + date);
+        System.out.println("salary at that time was equal to " + prevSalary);
+        prevSalary = salary;
+        date = LocalDateTime.now();
+        System.out.println("and now it is " + salary);
+        System.out.println();
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,25 +1,31 @@
 package com.company;
 
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+import java.util.concurrent.TimeUnit;
+
+import static java.util.concurrent.TimeUnit.*;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         Human me = new Human();
         me.fName = "Filip";
         me.lName = "Piotrowicz";
-
+/*
         me.car = new Car("ford", "mustang", 1969);
         me.car.plates = "123456";
         me.car.colour = "black";
         me.car.showMyCar();
-
-
-
-
-
-
-
-
+*/
+        me.getSalary();
+        //tu sprawdzam tylko czy dziala
+        me.setSalary(120.0);
+        SECONDS.sleep(10);
+        me.getSalary();
+        SECONDS.sleep(10);
+        me.getSalary();
 
 
     }
