23c6b4344038d86e52ec49d64056427c86abbcb4
frpiotrowicz <62611565+frpiotrowicz@users.noreply.github.com>
2020-05-05 03:33:42 +0200

    zadanie 6 czesc pierwsza

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -15,15 +15,24 @@ public class Main {
         me.lName = "Piotrowicz";
 
         Car car = new Car("ford", "mustang", 1969);
+        Car car1 = new Car("ford", "mustang", 1969);
+
+        System.out.println(car == car1);
+        System.out.println(car.equals(car1));
+
+        Car x = car;
+        System.out.println(car.equals(x));
+        System.out.println(car == x);
+        System.out.println(new Car("ford", "mustang", 1969).equals( new Car("ford", "mustang", 1969)));
+ /*
         car.value = 120.0;
         me.setCar(car);
         me.getCar().plates = "123456";
         me.getCar().colour = "black";
         me.getCar().showMyCar();
-
-
-
-
+*/
+        System.out.println(car);
+        System.out.println(car1);
 
     }
 }
